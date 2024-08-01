cc3b04ea1f82976e9ed58182025a21ed3ac8a657
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-01 17:13:49 +0100

    init

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello world!' });
+  });
+
+  app.get('/contacts', async (req, res) => {});
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+//
