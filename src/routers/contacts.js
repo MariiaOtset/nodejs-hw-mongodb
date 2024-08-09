c4e00e696333b015d95377b753012a9f50587f9a
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-09 15:50:24 +0100

    initial

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,14 +8,16 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { validateBody } from '../middlewares/vallidateBody.js';
+import { createContactSchema, updateContactSchema} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/', ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema),  ctrlWrapper(createContactController));
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
-router.put('/:contactId', ctrlWrapper(upsertContactController));
-router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.put('/:contactId',  validateBody(createContactSchema), ctrlWrapper(upsertContactController));
+router.patch('/:contactId',  validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
