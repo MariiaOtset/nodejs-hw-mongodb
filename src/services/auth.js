fc0ffb47e017fa2225593451f7b991f13843e86a
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-22 12:56:51 +0100

    reset password service created

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,8 +4,8 @@ import jwt from 'jsonwebtoken';
 import { randomBytes } from 'crypto';
 import handlebars from 'handlebars';
 import path from 'node:path';
-import fs from 'node:fs/promises';;
-import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
+import fs from 'node:fs/promises';
+import { FIFTEEN_MINUTES, THIRTY_DAYS, SMTP } from '../constants/index.js';
 import { SessionsCollection } from '../db/models/session.js';
 import { UsersCollection } from '../db/models/user.js';
 import { env } from '../utils/env.js';
@@ -85,22 +85,68 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
 };
 
 export const requestResetToken = async (email) => {
- const user = await UsersCollection.findOne({ email });
+  const user = await UsersCollection.findOne({ email });
 
- if(!user) {
-  throw createHttpError(404, 'User not found');
- }
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
 
- const resetToken = jwt.sign({
-  {
-    sub: user._id,
-    email,
-  },
-  env('JWT_SECRET'),
- });
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
+  );
+
+  const templateSource = await fs
+    .readFile(resetPasswordTemplatePath)
+    .toString();
+
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.name,
+    link: `${env('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+  });
+
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html,
+  });
 };
 
-// export const resetPassword = async (email) => {
-//   const
-// };
+export const resetPassword = async (payload) => {
+  let entries;
 
+  try {
+    entries = jwt.verify(payload.token, env('JWT_SECRET'));
+  } catch (error) {
+    if (error instanceof Error) throw createHttpError(401, error.message);
+    throw error;
+  }
+
+  const user = await UsersCollection.findOne({
+    email: entries.email,
+    _id: entries.sub,
+  });
+
+  if (!user) createHttpError(404, 'User not found');
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  await UsersCollection.updateOne(
+    { _id: user._id },
+    { password: encryptedPassword },
+  );
+};
