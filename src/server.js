ca2550f458dadaa7106befc539374b5e6d3633d3
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-22 20:25:28 +0100

    photo uploader added

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3001'));
 
@@ -21,7 +22,10 @@ export const setupServer = () => {
       },
     }),
   );
+
   app.use(cookieParser());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello world!' });
