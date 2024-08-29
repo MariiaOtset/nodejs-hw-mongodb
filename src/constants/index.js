dbd0fbaeae7df9811e65ed8b783116302678fb30
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-29 13:19:09 +0100

    documentation for all breakpoints successfully created

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,16 +21,11 @@ export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
-// export const CLOUDINARY = {
-//   CLOUD_NAME: 'CLOUD_NAME',
-//   API_KEY: 'API_KEY',
-//   API_SECRET: 'API_SECRET',
-//   ENABLED: 'ENABLE_CLOUDINARY',
-// };
-
 export const CLOUDINARY = {
   ENABLED: 'ENABLE_CLOUDINARY',
   CLOUD_NAME: 'CLOUDINARY_NAME',
   API_KEY: 'CLOUDINARY_API_KEY',
   API_SECRET: 'CLOUDINARY_API_SECRET',
 };
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
